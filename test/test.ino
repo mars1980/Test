ccd1575fd1513a05b42fc758c2ae8a7093febe4a
mars1980 <cm2878@nyu.edu>
2014-11-25 19:05:20 -0500

    added mult sensor serial

--- test/test.ino
+++ test/test.ino
@@ -1,13 +1,23 @@
 
 
-void setup() {
-  
-Serial.begin(9600);
+void setup()
+{
+  Serial.begin(9600);
+  // 1. write the first byte to processing...
+  Serial.write('0'); 
 }
 
-void loop() {
-  int photocell = analogRead(A0);
-  
-  Serial.print("photocell reads at: ");
-  Serial.println(photocell);
+void loop()
+{
+  //3. if processing sends anthing, then read the sensor
+  if (Serial.available() > 0)
+  {
+    //this will clear the buffer
+    char input = Serial.read();
+    int val = analogRead(A0)/4;
+
+    //4. send the sensor data back over to processing
+    Serial.write(val);
+  }
 }
+
